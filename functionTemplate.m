bf5253be7cad98101e212220c52ff5f6a9f60812
Justin Ales <justin.ales@gmail.com>
2016-06-21 15:36:34 +0100

    Created template structure

---
+++ functionTemplate.m
@@ -0,0 +1,9 @@
+function [y] = functionTemplate( x )
+%FUNCTIONTEMPLATE This is an example function to modify
+%   This funciton returns 2*x+2; 
+%   You should modify this function and save it with a new name.
+
+y = 2*x+2;
+
+end
+
---
+++ masterScript.m
@@ -0,0 +1,18 @@
+%% Initialize variables
+close all
+x = linspace(0,1,100); %Simple variable for our x axis
+
+
+
+%% Justin's Section
+
+figure;
+
+y = functionTemplate(x);
+
+plot(x,y);
+
+
+%% [INSERT NAME] Section 
+
+
