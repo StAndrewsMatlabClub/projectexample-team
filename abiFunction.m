1331389e3ebc6e9ee9419933af06c6fe63e688cd
abigailrilee <abigailrilee@googlemail.com>
2016-06-22 13:09:57 +0100

    Edits to abigailrilee section
    also editing function to work properly

--- abiFunction.m
+++ abiFunction.m
@@ -1,9 +1,9 @@
-function [y] = functionTemplate( x )
+function [y] = abiFunction( x )
 %FUNCTIONTEMPLATE This is an example function to modify
 %   This funciton returns 2*x+2; 
 %   You should modify this function and save it with a new name.
 
-y = 2*(x^2)+2;
+y = 2*(x.^2)+2;
 
 end
 
