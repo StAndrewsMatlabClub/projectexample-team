179d83926f3fd22aad7340b50346f8e2b754401b
j-urquhart <jau2@st-andrews.ac.uk>
2016-06-22 13:09:12 +0100

    Changed function template

--- functionTemplate.m
+++ functionTemplate.m
@@ -1,9 +1,9 @@
-function [y] = functionTemplate( x )
+function [y] = functionJosie( x )
 %FUNCTIONTEMPLATE This is an example function to modify
 %   This funciton returns 2*x+2; 
 %   You should modify this function and save it with a new name.
 
-y = 2*x+2;
+y = 5*x+2;
 
 end
 
